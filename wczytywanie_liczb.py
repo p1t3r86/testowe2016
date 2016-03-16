8de387385fb72fb4a7b904862f71d3b513f08111
piotr_s <testowy@test.com>
2016-03-16 13:02:54 +0100

    zmodyfikowana wersja

--- wczytywanie_liczb.py
+++ wczytywanie_liczb.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+print 'program prosi o liczbe,(potwierdz enterem) nastepnie ile razy ma byc wyswietlona'
 
 a=int(raw_input())
+i=int(raw_input())
 
-for i in range (10):
+for i in range (0,i):
 
 	print a
